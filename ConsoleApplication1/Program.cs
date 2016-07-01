301e84866ed51f01fb94776de40ac5060744740b
wanggangqq <1226930734@qq.com>
2016-07-01 10:29:27 +0800

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+     
+    }
+    public interface Index
+    {
+
+        void a(string n);
+    }
+    class a : Index {
+
+        public void a(string n)
+        {
+            Console.WriteLine(n);
+        }
+    }
+    
+}
